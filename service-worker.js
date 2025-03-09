dc0547ee94dedba0adf3e6f435bdc3fad1c45b19
The_Tech_Dog <jaredcooper705@gmail.com>
2025-03-09 12:23:49 +0000

    s

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'freedom-browser-cache-dynamic22';
+const CACHE_NAME = 'freedom-browser-cache-dynamic23';
 const OFFLINE_PAGE = '/offline-test2/offline.html';
 
 // Install event: Cache the offline page initially
